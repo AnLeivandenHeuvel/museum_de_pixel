4e4c55450ecd6e295968ba06c346f4ceabcc5037
AnLeivandenHeuvel <102820@glr.nl>
2024-10-03 08:06:39 +0000

    img

--- an_lei/javascript.js
+++ an_lei/javascript.js
@@ -1,2 +1,6 @@
 document.write('<img src="img/Galaga_logo.png" alt="logo van Galaga">');
-
+document.write('<img src="img/ship_1.png" class="img1" alt="galaga ship_1">');
+document.write('<img src="img/ship_2.png" class="img2" alt="galaga ship_2">');
+document.write('<img src="img/ship_3.png" class="img3" alt="Galaga ship_3">');
+document.write('<img src="img/vijand_2.gif" class="img4" alt="vijand_2" width="130" height="214">');
+document.write('<img src="img/vijand_1.gif" class="img5" alt="vijand_1" width="130" height="214">');
