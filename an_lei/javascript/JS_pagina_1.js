b8abb11d1d9ec1d19dbda1d5994af7d2324d21a8
AnLeivandenHeuvel <102820@glr.nl>
2024-10-06 13:41:05 +0000

    pagina 2

--- an_lei/javascript/JS_pagina_1.js
+++ an_lei/javascript/JS_pagina_1.js
@@ -1,6 +1,8 @@
-document.write('<img src="img/Galaga_logo.png" alt="logo van Galaga">');
+document.write('<a href="pagina_1.html">')
+document.write('<img src="img/Galaga_logo.png"  alt="logo van Galaga">');
+document.write('</a>')
 document.write('<img src="img/ship_1.png" class="img1" alt="galaga ship_1">');
 document.write('<img src="img/ship_2.png" class="img2" alt="galaga ship_2">');
 document.write('<img src="img/ship_3.png" class="img3" alt="Galaga ship_3">');
 document.write('<img src="img/vijand_2.gif" class="img4" alt="vijand_2" width="130" height="214">');
-
+document.write('<img src="img/NES-gameplay.gif" class="img6" alt="gameplay Galaga">')
---
+++ an_lei/javascript/JS_pagina_2.js
@@ -0,0 +1,3 @@
+document.write('<a href="pagina_1.html">')
+document.write('<img src="img/Galaga_logo.png"  alt="logo van Galaga">');
+document.write('</a>')
